4a36e438f1088aab44d05d9e06d754d471dc410d
Brad Wrigley <bmichaelwrigley@gmail.com>
2026-01-11 18:02:06 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -1,12 +1,6 @@
-import Home from './pages/Home';
-import Library from './pages/Library';
-import Admin from './pages/Admin';
 
 
 export const PAGES = {
-    "Home": Home,
-    "Library": Library,
-    "Admin": Admin,
 }
 
 export const pagesConfig = {
